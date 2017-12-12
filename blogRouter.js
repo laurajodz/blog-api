bd5c0347f60bb0d6a25f12e6e544cab1f36fb055
Laura Jodz <laurajodz@gmail.com>
2017-12-12 12:41:49 -0800

    final

--- blogRouter.js
+++ blogRouter.js
@@ -4,18 +4,18 @@ const router = express.Router();
 const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
 
-const {Blog} = require('./models');
+const {BlogPosts} = require('./models');
 
 // we're going to add some blogs
 // so there's some data to look at
-Blog.create('Trip to Philly', 'I arrive on December 25', 'Laura Jodz', '12-12-2017');
-Blog.create('Trip to Cape Rey', 'We travelled here for Ken\'s birthday', 'Laura Jodz', '12-12-2017');
-Blog.create('Trip to Texas', 'Ken leaves on December 19', 'Laura Jodz', '12-11-2017');
+BlogPosts.create('Trip to Philly', 'I arrive on December 25', 'Laura Jodz');
+BlogPosts.create('Trip to Cape Rey', 'We travelled here for Ken\'s birthday', 'Laura Jodz');
+BlogPosts.create('Trip to Texas', 'Ken leaves on December 19', 'Laura Jodz');
 
 // when the root of this router is called with GET, return
 // all current Blog entrys
-router.get('/blog-posts', (req, res) => {
-  res.json(Blog.get());
+router.get('/', (req, res) => {
+  res.json(BlogPosts.get());
 });
 
 
@@ -23,7 +23,7 @@ router.get('/blog-posts', (req, res) => {
 // got required fields. if not, log an error and
 // return a 400 status code. if okay,
 // add new entry to Blog and return it with a 201.
-router.post('/blog-posts', jsonParser, (req, res) => {
+router.post('/', jsonParser, (req, res) => {
   const requiredFields = ['title', 'content', 'author'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
@@ -33,15 +33,15 @@ router.post('/blog-posts', jsonParser, (req, res) => {
       return res.status(400).send(message);
     }
   }
-  const entry = Blog.create(req.body.title, req.body.content, req.body.author, req.body.date);
+  const entry = BlogPosts.create(req.body.title, req.body.content, req.body.author);
   res.status(201).json(entry);
 });
 
 
 // when DELETE request comes in with an id in path,
 // delete that item from Blog.
-router.delete('/blog-posts/:id', (req, res) => {
-  Blog.delete(req.params.id);
+router.delete('/:id', (req, res) => {
+  BlogPosts.delete(req.params.id);
   console.log(`Deleted Blog entry \`${req.params.id}\``);
   res.status(204).end();
 });
@@ -51,12 +51,12 @@ router.delete('/blog-posts/:id', (req, res) => {
 // entry id in updated item object match. if problems with any
 // of that, log error and send back status code 400. otherwise
 // call `Blog.update` with updated entry.
-router.put('/blog-posts/:id', jsonParser, (req, res) => {
-  const requiredFields = ['title', 'content', 'author'];
+router.put('/:id', jsonParser, (req, res) => {
+  const requiredFields = ['id', 'title', 'content', 'author', 'publishDate'];
   for (let i=0; i<requiredFields.length; i++) {
     const field = requiredFields[i];
     if (!(field in req.body)) {
-      const message = `\`${field}\` is required`
+      const message = `The \`${field}\` is required`;
       console.error(message);
       return res.status(400).send(message);
     }
@@ -69,11 +69,12 @@ router.put('/blog-posts/:id', jsonParser, (req, res) => {
     return res.status(400).send(message);
   }
   console.log(`Updating blog entry \`${req.params.id}\``);
-  const updatedEntry = Blog.update({
-    title: req.params.id,
-    content: req.body.name,
-    author: req.body.budget,
-    date: req.body.date
+  const updatedEntry = BlogPosts.update({
+    id: req.params.id,
+    title: req.body.title,
+    content: req.body.content,
+    author: req.body.author,
+    publishDate: req.body.publishDate
   });
   res.status(204).end();
 })
--- server.js
+++ server.js
@@ -1,13 +1,12 @@
 const express = require('express');
 const morgan = require('morgan');
 
-const app = express();
-
 const blogRouter = require('./blogRouter');
 
+const app = express();
+
 app.use(morgan('common'));
-app.use(express.static('public'));
-app.use('/', blogRouter);
+app.use('/blog-posts', blogRouter);
 
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
